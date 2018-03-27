5bfbec895159e95064fbe062dded89046dba7beb
raghavyadavm <raghavyadav258@gmail.com>
2018-03-27 14:55:32 -0500

    removed unused variables and logic

--- worker.js
+++ worker.js
@@ -1,19 +1,26 @@
 var SCWorker = require('socketcluster/scworker');
-var fs = require('fs');
 var express = require('express');
 var serveStatic = require('serve-static');
 var path = require('path');
-var os = require('os');
+var morgan = require('morgan');
+var healthChecker = require('sc-framework-health-check');
 
 class Worker extends SCWorker {
   run() {
     console.log('   >> Worker PID:', process.pid);
+    var environment = this.options.environment;
 
     var app = express();
     var clientsList = [];
 
     var httpServer = this.httpServer;
     var scServer = this.scServer;
+
+    if (environment === 'dev') {
+      // Log every HTTP request. See https://github.com/expressjs/morgan for other
+      // available formats.
+      app.use(morgan('dev'));
+    }
 
     app.use(serveStatic(path.resolve(__dirname, 'public')));
 
@@ -24,13 +31,14 @@ class Worker extends SCWorker {
     */
     scServer.on('connection', function (server) {
       console.log('User connected');
+      console.log('clients connected are', Object.keys(scServer.clients));
       console.log('id: ', server.id);
-      clientsList.push(server.id);
-      console.log("list of clients", clientsList);
+      // clientsList.push(server.id);
+      // console.log("list of clients", clientsList);
 
-      //scServer.exchange.publish('clientsListBroadcast', clientsList);
+      scServer.exchange.publish('clientsConnected', Object.keys(scServer.clients));
 
-      console.log('clients count: ', scServer.clientsCount);
+      // console.log('clients count: ', scServer.clientsCount);
 
       server.on('msg', function (message) {
         console.log('client said: ', message);
@@ -62,7 +70,7 @@ class Worker extends SCWorker {
           scServer.exchange.publish('join', data); //io.sockets.in(room).emit('join', room);
           //scServer.exchange.publish('isInitiator', data); //io.sockets.in(room).emit('join', room);
           server.emit('askClientToSubscribe', data.room); //socket.join(room);
-          server.emit('joined', clientsList);
+          server.emit('joined', Object.keys(scServer.clients));
           //server.emit('full', room);
         }
       });
@@ -108,14 +116,35 @@ class Worker extends SCWorker {
       });
 
       server.on('disconnect', function () {
-        console.log('User disconnected');
+        console.log('User disconnected ', server.id);
+        // var index = clientsList.indexOf(server.id);
+        // if (index !== -1) clientsList.splice(index, 1);
+        // console.log('new clients list', clientsList);
+        console.log('disconnection ',Object.keys(scServer.clients));
+        scServer.exchange.publish('clientsDisconnect', Object.keys(scServer.clients));
+        scServer.exchange.publish('removeVideo', server.id);
+        // scServer.exchange.publish('yell', server.id+' got disconnected');
       });
 
       server.on('screenOffer', function(data) {
         console.log('screenOffer ', data);
         scServer.exchange.publish('screenOffer', data);
-      });    
+      });
 
+      server.on('screenAnswer', function(data) {
+        console.log('screenAnswer ',data);
+        scServer.exchange.publish('screenAnswer', data);
+      });
+
+      server.on('screenIceAnswer', function(data) {
+        console.log('screenIceAnswer ',data);
+        scServer.exchange.publish('screenIceAnswer', data);
+      });
+
+      server.on('screenIceOffer', function(data) {
+        console.log('screenIceOffer ',data);
+        scServer.exchange.publish('screenIceOffer', data);
+      });
     });
   }
 }
